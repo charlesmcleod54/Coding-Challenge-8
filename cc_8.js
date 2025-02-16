2b795d068b2e4ceeadcb356035e569f93a8bdf22
Charles McLeod <charlesmcleod@usf.edu>
2025-02-16 17:16:20 -0500

    Task 5 - Loan Payment Calculation

--- cc_8.js
+++ cc_8.js
@@ -36,3 +36,12 @@ function calculateRentalCost(days, carType, insurance = false) {
 
 console.log(calculateRentalCost(3, "Economy", true)); // Expected output: "Total Rental Cost: $180"
 console.log(calculateRentalCost(5, "Luxury", false)); // Expected output: "Total Rental Cost: $500"
+
+// Task 5
+function calculateLoanPayment(principal, rate, time) {
+    let totalPayment = principal + (principal * rate * time);
+    return 'Total Payment: $${totalPayment.toFixed(2)}';
+}
+
+console.log(calculateLoanPayment(1000, 0.05, 2)); // Expected output: "Total Payment: $1100.00"
+console.log(calculateLoanPayment(5000, 0.07, 3)); // Expected output: "Total Payment: $6050.00"
